c86858e8bab5e91186c726a5867491d52060e71f
Prafull Epili <prafullepili@gmail.com>
2023-07-19 17:48:17 +0530

    local db connect added

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI_LOCAL);
 
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
