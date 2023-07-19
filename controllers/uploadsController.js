661a5fee68e2565bc80fc50188137a3e1e3106ff
Prafull Epili <prafullepili@gmail.com>
2023-07-19 18:35:25 +0530

    uploadProductImage controller added

---
+++ controllers/uploadsController.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { StatusCodes } = require('http-status-codes');
+
+
+const uploadProductImage = async (req, res) => {
+    res.send('uploadProductImage')
+};
+
+module.exports = {
+    uploadProductImage
+}
