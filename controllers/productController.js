d2c70f99e4427df1c92ea9c3fe5525aeb4b84275
Prafull Epili <prafullepili@gmail.com>
2023-07-19 18:34:51 +0530

    createProduct, getAllProducts controller added

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+
+
+const createProduct = async (req, res) => {
+    req.send('create product')
+};
+
+const getAllProducts = async (req, res) => {
+    res.send("list of products");
+};
+
+module.exports = {
+    createProduct,
+    getAllProducts
+}
