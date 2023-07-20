e7fd58408b98747275ba6efdfe4d75d1d1606ffe
Prafull Epili <prafullepili@gmail.com>
2023-07-20 12:06:58 +0530

    UI checks

--- public/browser-app.js
+++ public/browser-app.js
@@ -17,61 +17,74 @@ let imageValue;
 
 
 
-
-
-imageInputDOM.addEventListener('change',async (e)=>{
- const imageFile = e.target.files[0];
- const formData = new FormData();
- formData.append('image',imageFile)
- try {
-  const {data:{image:{src}}} = await axios.post(`${url}/uploads`,formData,{
-   headers:{
-    'Content-Type':'multipart/form-data'
-   }
-  })
-  imageValue = src
- } catch (error) {
-   imageValue = null
-  console.log(error);
- }
-})
-
-
-fileFormDOM.addEventListener('submit',async (e)=>{
-e.preventDefault()
-const nameValue = nameInputDOM.value;
-const priceValue = priceInputDOM.value;
-try {
- 
- const product = {name:nameValue,price:priceValue,image:imageValue}
- 
-  await axios.post(url,product);
-  fetchProducts()
-} catch (error) {
- console.log(error);
+function errorHandle(imageFile) {
+  let small = document.getElementById('img-error');
+  if (imageFile == undefined) {
+    small.append('Please select image file')
+    small.style.color = 'red';
+  } else {
+    small.innerText = '';
+  }
 }
+
+imageInputDOM.addEventListener('change', async (e) => {
+  const imageFile = e.target.files[0];
+  errorHandle(imageFile)
+})
+
+const uploadImage = async (e) => {
+  const imageFile = e.currentTarget[2].files[0]
+  const formData = new FormData();
+  formData.append('image', imageFile);
+  imageFile == undefined && errorHandle(imageFile)
+  try {
+    const { data: { image: { src } } } = await axios.post(`${url}/uploads`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    return src
+  } catch (error) {
+    console.log(error);
+    return src
+  }
+}
+
+fileFormDOM.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const nameValue = nameInputDOM.value;
+  const priceValue = priceInputDOM.value;
+  const imageValue = await uploadImage(e);
+  if (imageValue == null) return;
+  try {
+    const product = { name: nameValue, price: priceValue, image: imageValue }
+    await axios.post(url, product);
+    fetchProducts()
+  } catch (error) {
+    console.log(error);
+  }
 })
 
 
 
-async function fetchProducts () {
- try {
-  const {data:{products}} = await axios.get(url);
-  
-  const productsDOM = products.map((product)=>{
-return `<article class="product">
+async function fetchProducts() {
+  try {
+    const { data: { products } } = await axios.get(url);
+
+    const productsDOM = products.map((product) => {
+      return `<article class="product">
 <img src="${product.image}" alt="${product.name}" class="img"/>
 <footer>
 <p>${product.name}</p>
 <span>$${product.price}</span>
 </footer>
 </article>`
-  }).join('')
-  containerDOM.innerHTML = productsDOM
- } catch (error) {
-  console.log(error);
- }
- 
+    }).join('')
+    containerDOM.innerHTML = productsDOM
+  } catch (error) {
+    console.log(error);
+  }
+
 }
 
 fetchProducts()
